e4920aeefd1e6305cc74ccc11dd9d76838443007
unknown <dorahuairesolis@gmail.com>
2024-08-30 18:41:38 -0500

    implementar primeros cambios de estilo en la página apikey

--- src/component/componentApi.js
+++ src/component/componentApi.js
@@ -4,7 +4,7 @@ export const renderApi = () => {
       
   contenedorApi.innerHTML = 
         `
-          <h1>Api Key Admin</h1>
+          <h1 class = "titulo-api" >Api Key Admin</h1>
           <p>Desde aquí puedes administrar la API Key a utilizar</p>
           <input type = "password" placeholder = "Escribe/pega tu API KEY aquí"></input>
           <button>Guardar</button>
